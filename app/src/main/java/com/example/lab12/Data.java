9aad3586180fd1aa9ee64052cd55f15798acb1b9
張耀仁 <t110650031@ntut.org.tw>
2024-12-20 15:17:45 +0800

    first commit

---
+++ app/src/main/java/com/example/lab12/Data.java
@@ -0,0 +1,16 @@
+package com.example.lab12;
+
+import java.sql.ResultSet;
+
+public class Data {
+    Result result;
+    static class Result{
+        Results[]results;
+        static class  Results{
+
+
+        String Station;
+        String Destination;
+        }
+    }
+}
